befeefac571e0ed530112a644f31339083ab3735
Pietro Lauria <ptr.lauria@gmail.com>
2022-01-06 12:29:18 +0100

    10 115

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 export const ImageList = props => {
   console.log(props.images);
-  return <div>Imagelist </div>;
+
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  return <div className='image-list'>{images} </div>;
 };
